f2be8dce8074b4f32d0551ed113a542cbbb9718e
linlinkyaw388 <linlinkyaw388@gmail.com>
2026-05-30 07:59:29 +0630

    auth

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//User Auth
+Auth::routes();
+
+
+//Admin User Auth
+Route::get('admin/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+Route::post('admin/login', 'Auth\AdminLoginController@login')->name('admin.login');
+
+Route::get('/', 'Frontend\PageController@home');
